fc6ba55ebc2afbfb8a5a7b998ac5f924249fd255
Ogushanakin <ogzhnakn96@gmail.com>
2022-10-18 15:27:58 +0300

    created Project

---
+++ Ogus Chat/Ogus Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ogus Chat
+//
+//  Created by AKIN on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
