9e82b957fec31790bf2e90e0b495dd6529066450
Ogushanakin <ogzhnakn96@gmail.com>
2022-10-20 08:00:29 +0300

    Setup Firebase & Create User

--- Ogus Chat/Ogus Chat/Controller/Authentication/RegistrationController.swift
+++ Ogus Chat/Ogus Chat/Controller/Authentication/RegistrationController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 final class RegistrationController: UIViewController {
     
     // MARK: - Properties
     
     private var viewModel = RegisterViewModel()
+    private var profileImage: UIImage?
     
     private lazy var plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
@@ -62,7 +65,7 @@ final class RegistrationController: UIViewController {
         button.backgroundColor = #colorLiteral(red: 0.158723802, green: 0.2352941176, blue: 0.4941176471, alpha: 0.656379346)
         button.setHeight(height: 50)
         button.isEnabled = false
-        button.addTarget(self, action: #selector(handleSignup), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handleRegistration), for: .touchUpInside)
         return button
     }()
     
@@ -91,8 +94,50 @@ final class RegistrationController: UIViewController {
     
     // MARK: - Selectors
     
-    @objc func handleSignup() {
+    @objc func handleRegistration() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let fullname = fullnameTextField.text else { return }
+        guard let username = usernameTextField.text?.lowercased() else { return }
+        guard let profileImage = profileImage else { return }
         
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        
+        let filename = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images/\(filename)")
+        
+        ref.putData(imageData, metadata: nil) { (meta, error) in
+            if let error = error {
+                print("DEBUG: Failed to upload image with error \(error.localizedDescription)")
+                return
+            }
+            
+            ref.downloadURL { (url, error) in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    if let error = error {
+                        print("DEBUG: Failed to create user with error \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let uid = result?.user.uid else { return }
+                    
+                    let data = ["email": email,
+                                "fullname": fullname,
+                                "profileImageUrl": profileImageUrl,
+                                "uid": uid,
+                                "username": username] as [String : Any]
+                    Firestore.firestore().collection("users").document(uid).setData(data) { error in
+                        if let error = error {
+                            print("DEBUG: Failed to create user with error \(error.localizedDescription)")
+                            return
+                        }
+                        print("DEBUG:created user....")
+                    }
+                }
+            }
+        }
     }
     
     @objc func handleSelectPhoto() {
@@ -161,6 +206,7 @@ final class RegistrationController: UIViewController {
 extension RegistrationController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.originalImage] as? UIImage
+        profileImage = image
         plusPhotoButton.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
         plusPhotoButton.layer.borderColor = UIColor.white.cgColor
         plusPhotoButton.layer.borderWidth = 3.0
